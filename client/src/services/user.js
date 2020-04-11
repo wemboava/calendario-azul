36ba2c82fde6b65dbdb913cae3b5c3955bb15037
willian emboava <w.emboava.o@gmail.com>
2020-04-11 20:04:27 -0300

    created update user method

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -4,6 +4,9 @@ class UserService extends BaseService {
   create (data) {
     return this.post(`${this.getUrl()}/users`, data)
   }
+  update (data) {
+    return this.post(`${this.getUrl()}/users`, data)
+  }
 }
 
 export default new UserService
